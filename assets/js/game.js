ef83eebdbd8660a1fe21b2498b3659d0ca55c32a
Kash <kashay@agyeman.me>
2020-12-24 10:33:46 -0700

    robots now attack each other once, w/ prompt windows with health updates and console logs

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,13 +1,48 @@
-// this creates a function named "fight"
-
+// Variables to fight
 var playerName = window.prompt("What is your robot's name?");
-console.log(playerName);
-console.log("This logs a string, good for leaving yourself a message");
-console.log(10 + 10);
-console.log("Our robot's name is " + playerName);
+var playerHealth = 100;
+var playerAttack = 10;
 
-function fight() {
-  window.alert("The fight has begun!");
+var enemyName = "Roborto";
+var enemyHealth = 50; 
+var enemyAttack = 12;
+
+//Variables log check pre fight.
+console.log(playerName, playerHealth, playerAttack);
+console.log(enemyName, enemyHealth, enemyAttack);
+
+//Fight function 
+var fight = function() {
+  window.alert("welcome to Robot Gladiators!");
+  enemyHealth = enemyHealth - playerAttack;
+  playerHealth = playerHealth - enemyAttack;
+};
+
+fight();
+
+//Post Fight enemyHealth variable log check
+console.log (
+  playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining "
+)
+
+//check enemy's health
+if (enemyHealth <= 0) {
+  window.alert(enemyName + " had died!");
+}
+else {
+  window.alert(enemyName + " still has " + enemyHealth + " health left.");
 }
 
-// fight();
+//Post fight playerHealth variable log check
+console.log (
+  enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining "
+)
+
+//check Players health
+if (playerHealth <= 0) {
+  window.alert(playerName + " has died!");
+}
+else {
+  window.alert(playerName + " still has " + playerHealth + " health left.");
+}
+
